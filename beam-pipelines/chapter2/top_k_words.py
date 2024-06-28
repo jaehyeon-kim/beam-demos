a872e14e235cef1b9c3358af2a3ffca972c4a310
Jaehyeon Kim <dottami@gmail.com>
2024-06-28 13:04:49 +1000

    update top k words

--- beam-pipelines/chapter2/top_k_words.py
+++ beam-pipelines/chapter2/top_k_words.py
@@ -1,5 +1,4 @@
 import os
-import datetime
 import argparse
 import json
 import re
@@ -7,7 +6,9 @@ import typing
 import logging
 
 import apache_beam as beam
+from apache_beam import pvalue
 from apache_beam.io import kafka
+from apache_beam.transforms.window import FixedWindows
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
 
@@ -21,106 +22,155 @@ def tokenize(element: str):
     return re.findall(r"[A-Za-z\']+", element)
 
 
-def create_message(element: typing.Tuple[datetime.datetime, datetime.datetime, float]):
-    msg = json.dumps(
-        {
-            "window_start": element[0].isoformat(timespec="seconds"),
-            "window_end": element[1].isoformat(timespec="seconds"),
-            "avg_len": element[2],
-        }
-    )
+def create_message(element: typing.Tuple[str, str, str, int]):
+    msg = json.dumps(dict(zip(["window_start", "window_end", "word", "freq"], element)))
     print(msg)
     return "".encode("utf-8"), msg.encode("utf-8")
 
 
 class AddWindowTS(beam.DoFn):
-    def process(self, avg_len: float, win_param=beam.DoFn.WindowParam):
+    def process(self, top_k: typing.Tuple[str, int], win_param=beam.DoFn.WindowParam):
         yield (
-            win_param.start.to_utc_datetime(),
-            win_param.end.to_utc_datetime(),
-            avg_len,
+            win_param.start.to_rfc3339(),
+            win_param.end.to_rfc3339(),
+            top_k[0],
+            top_k[1],
         )
 
 
-def run():
+class ReadInputsFromKafka(beam.PTransform):
+    def __init__(
+        self,
+        bootstrap_servers: str,
+        topics: typing.List[str],
+        group_id: str,
+        verbose: bool = False,
+        expansion_service: typing.Any = None,
+        label: str | None = None,
+    ) -> None:
+        super().__init__(label)
+        self.boostrap_servers = bootstrap_servers
+        self.topics = topics
+        self.group_id = group_id
+        self.verbose = verbose
+        self.expansion_service = expansion_service
+
+    def expand(self, input: pvalue.PBegin):
+        return (
+            input
+            | "ReadFromKafka"
+            >> kafka.ReadFromKafka(
+                consumer_config={
+                    "bootstrap.servers": self.boostrap_servers,
+                    "auto.offset.reset": "latest",
+                    # "enable.auto.commit": "true",
+                    "group.id": self.group_id,
+                },
+                topics=self.topics,
+                timestamp_policy=kafka.ReadFromKafka.create_time_policy,
+                commit_offset_in_finalize=True,
+                expansion_service=self.expansion_service,
+            )
+            | "DecodeMessage" >> beam.Map(decode_message)
+        )
+
+
+class CalculateTopKWords(beam.PTransform):
+    def __init__(
+        self, window_length: int, top_k: int, label: str | None = None
+    ) -> None:
+        self.window_length = window_length
+        self.top_k = top_k
+        super().__init__(label)
+
+    def expand(self, input: pvalue.PCollection):
+        return (
+            input
+            | "Windowing" >> beam.WindowInto(FixedWindows(size=self.window_length))
+            | "Tokenize" >> beam.FlatMap(tokenize)
+            | "CountPerWord" >> beam.combiners.Count.PerElement()
+            | "TopKWords"
+            >> beam.combiners.Top.Of(self.top_k, lambda e: e[1]).without_defaults()
+            | "Flatten" >> beam.FlatMap(lambda e: e)
+        )
+
+
+class WriteOutputsToKafka(beam.PTransform):
+    def __init__(
+        self,
+        bootstrap_servers: str,
+        topic: str,
+        expansion_service: typing.Any = None,
+        label: str | None = None,
+    ) -> None:
+        super().__init__(label)
+        self.boostrap_servers = bootstrap_servers
+        self.topic = topic
+        self.expansion_service = expansion_service
+
+    def expand(self, input: pvalue.PCollection):
+        return (
+            input
+            | "AddWindowTS" >> beam.ParDo(AddWindowTS())
+            | "CreateMessages"
+            >> beam.Map(create_message).with_output_types(typing.Tuple[bytes, bytes])
+            | "WriteToKafka"
+            >> kafka.WriteToKafka(
+                producer_config={"bootstrap.servers": self.boostrap_servers},
+                topic=self.topic,
+                expansion_service=self.expansion_service,
+            )
+        )
+
+
+def run(argv=None, save_main_session=True):
     parser = argparse.ArgumentParser(description="Beam pipeline arguments")
-    parser.add_argument("--runner", default="FlinkRunner", help="Apache Beam runner")
     parser.add_argument(
-        "--use_own",
-        action="store_true",
-        default="Flag to indicate whether to use an own local cluster",
+        "--bootstrap_servers",
+        default="host.docker.internal:29092",
+        help="Kafka bootstrap server addresses",
     )
-    parser.add_argument("--length", default="5", type=int, help="Window length")
-    parser.add_argument("--top", default="3", type=int, help="Top k")
-    parser.add_argument("--input", default="input-topic", help="Input topic")
+    parser.add_argument("--input_topic", default="input-topic", help="Input topic")
     parser.add_argument(
-        "--job_name",
+        "--output_topic",
         default=re.sub("_", "-", re.sub(".py$", "", os.path.basename(__file__))),
-        help="Job name",
+        help="Output topic",
     )
-    opts = parser.parse_args()
-    print(opts)
+    parser.add_argument("--window_length", default="10", type=int, help="Window length")
+    parser.add_argument("--top_k", default="3", type=int, help="Top k")
 
-    pipeline_opts = {
-        "runner": opts.runner,
-        "job_name": opts.job_name,
-        "environment_type": "LOOPBACK",
-        "streaming": True,
-        "parallelism": 3,
-        "experiments": [
-            "use_deprecated_read"
-        ],  ## https://github.com/apache/beam/issues/20979
-        "checkpointing_interval": "60000",
-    }
-    if opts.use_own is True:
-        pipeline_opts = {**pipeline_opts, **{"flink_master": "localhost:8081"}}
-    print(pipeline_opts)
-    options = PipelineOptions([], **pipeline_opts)
-    # Required, else it will complain that when importing worker functions
-    options.view_as(SetupOptions).save_main_session = True
+    known_args, pipeline_args = parser.parse_known_args(argv)
+    print(f"known args - {known_args}")
+    print(f"pipeline args - {pipeline_args}")
 
-    p = beam.Pipeline(options=options)
-    (
-        p
-        | "Read from Kafka"
-        >> kafka.ReadFromKafka(
-            consumer_config={
-                "bootstrap.servers": os.getenv(
-                    "BOOTSTRAP_SERVERS",
-                    "host.docker.internal:29092",
-                ),
-                "auto.offset.reset": "earliest",
-                # "enable.auto.commit": "true",
-                "group.id": opts.job_name,
-            },
-            topics=[opts.input],
+    # We use the save_main_session option because one or more DoFn's in this
+    # workflow rely on global context (e.g., a module imported at module level).
+    pipeline_options = PipelineOptions(pipeline_args)
+    pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
+
+    with beam.Pipeline(options=pipeline_options) as p:
+        (
+            p
+            | "ReadInputsFromKafka"
+            >> ReadInputsFromKafka(
+                bootstrap_servers=known_args.bootstrap_servers,
+                topics=[known_args.input_topic],
+                group_id=f"{known_args.output_topic}-group",
+            )
+            | "CalculateTopKWords"
+            >> CalculateTopKWords(
+                window_length=known_args.window_length,
+                top_k=known_args.top_k,
+            )
+            | "WriteOutputsToKafka"
+            >> WriteOutputsToKafka(
+                bootstrap_servers=known_args.bootstrap_servers,
+                topic=known_args.output_topic,
+            )
         )
-        | "Decode messages" >> beam.Map(decode_message)
-        | "Windowing" >> beam.WindowInto(beam.window.FixedWindows(opts.length))
-        | "Extract words" >> beam.FlatMap(tokenize)
-        | "Count per word" >> beam.combiners.Count.PerElement()
-        | "Top k words"
-        >> beam.combiners.Top.Of(opts.top, lambda e: e[1]).without_defaults()
-        | "Flatten" >> beam.FlatMap(lambda e: e)
-        | "Add window timestamp" >> beam.ParDo(AddWindowTS())
-        | "Create messages"
-        >> beam.Map(create_message).with_output_types(typing.Tuple[bytes, bytes])
-        | "Write to Kafka"
-        >> kafka.WriteToKafka(
-            producer_config={
-                "bootstrap.servers": os.getenv(
-                    "BOOTSTRAP_SERVERS",
-                    "host.docker.internal:29092",
-                )
-            },
-            topic=opts.job_name,
-        )
-    )
 
-    logging.getLogger().setLevel(logging.INFO)
-    logging.info("Building pipeline ...")
-
-    p.run().wait_until_finish()
+        logging.getLogger().setLevel(logging.WARN)
+        logging.info("Building pipeline ...")
 
 
 if __name__ == "__main__":
